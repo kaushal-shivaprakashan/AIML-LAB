ea5b16fdfc362142cb1b8aab3c28208ba4458b1e
Kaushal Shivaprakash <86502925+kaushal-shivaprakashan@users.noreply.github.com>
2023-01-06 05:04:31 +0530

    Update PGM2.py

--- PGM2.py
+++ PGM2.py
@@ -1,111 +1,94 @@
-class Graph:
-    def __init__(self, graph, heuristicNodeList, startNode):  
+def computeMinimumCostChildNodes(n, h, g):
+    minCost = 0
+    
+    costToChildNode = {}
+    costToChildNode[minCost] = []
+    
+    flag = True
+    for nodeInfoTupleList in g[n]:
+        cost = 0
+        nodeList = []
         
-        self.graph = graph
-        self.H=heuristicNodeList
-        self.start=startNode
-        self.parent={}
-        self.status={}
-        self.solutionGraph={}
-     
-    def applyAOStar(self):        
-        self.aoStar(self.start, False)
-
-    def getNeighbors(self, v):     
-        return self.graph.get(v,'')
-    
-    def getStatus(self,v):        
-        return self.status.get(v,0)
-    
-    def setStatus(self,v, val):   
-        self.status[v]=val
-    
-    def getHeuristicNodeValue(self, n):
-        return self.H.get(n,0)     
- 
-    def setHeuristicNodeValue(self, n, value):
-        self.H[n]=value             
-        
-    
-    def printSolution(self):
-        print("FOR GRAPH SOLUTION, TRAVERSE THE GRAPH FROM THE START NODE:",self.start)
-        print("------------------------------------------------------------")
-        print(self.solutionGraph)
-        print("------------------------------------------------------------")
-    
-    def computeMinimumCostChildNodes(self, v):      
-        minimumCost=0
-        costToChildNodeListDict={}
-        costToChildNodeListDict[minimumCost]=[]
-        flag=True
-        for nodeInfoTupleList in self.getNeighbors(v):  
-            cost=0
-            nodeList=[]
-            for c, weight in nodeInfoTupleList:
-                cost=cost+self.getHeuristicNodeValue(c)+weight
-                nodeList.append(c)
+        for c, w in nodeInfoTupleList:
+            cost = cost + h[c] + w
+            nodeList.append(c)
             
-            if flag==True:                       
-                minimumCost=cost
-                costToChildNodeListDict[minimumCost]=nodeList     
-                flag=False
-            else:                                  
-                if minimumCost>cost:
-                    minimumCost=cost
-                    costToChildNodeListDict[minimumCost]=nodeList  
-                      
-        return minimumCost, costToChildNodeListDict[minimumCost]   
-                     
-    def aoStar(self, v, backTracking):     
-        
-        print("HEURISTIC VALUES  :", self.H)
-        print("SOLUTION GRAPH    :", self.solutionGraph)
-        print("PROCESSING NODE   :", v)
-        print("-----------------------------------------------------------------------------------------")
-        
-        if self.getStatus(v) >= 0:        
-            minimumCost, childNodeList = self.computeMinimumCostChildNodes(v)
-            self.setHeuristicNodeValue(v, minimumCost)
-            self.setStatus(v,len(childNodeList))
+        if flag == True:
+            minCost = cost
+            costToChildNode[minCost] = nodeList
+            flag=False
             
-            solved=True                      
-            for childNode in childNodeList:
-                self.parent[childNode]=v
-                if self.getStatus(childNode)!=-1:
-                    solved=solved & False
-            
-            if solved==True:             
-                self.setStatus(v,-1)    
-                self.solutionGraph[v]=childNodeList   
-            
-            
-            if v!=self.start:              
-                self.aoStar(self.parent[v], True)   
+        else:
+            if minCost > cost:
+                minCost = cost
+                costToChildNode[minCost] = nodeList
                 
-            if backTracking==False:      
-                for childNode in childNodeList:   
-                    self.setStatus(childNode,0)  
-                    self.aoStar(childNode, False)        
-                                       
-# h1 = {'A': 1, 'B': 6, 'C': 2, 'D': 12, 'E': 2, 'F': 1, 'G': 5, 'H': 7, 'I': 7, 'J': 1, 'T': 3}
-# graph1 = {
-#     'A': [[('B', 1), ('C', 1)], [('D', 1)]],
-#     'B': [[('G', 1)], [('H', 1)]],
-#     'C': [[('J', 1)]],
-#     'D': [[('E', 1), ('F', 1)]],
-#     'G': [[('I', 1)]]   
-# }
-# G1= Graph(graph1, h1, 'A')
-# G1.applyAOStar() 
-# G1.printSolution()
+    return minCost, costToChildNode[minCost]
+def aostar(status, h, graph, solutionGraph, n, backTracking):
+    
+    print("HEURISTIC VALUES :", h)
+    print("SOLUTION GRAPH :", solutionGraph)
+    print("PROCESSING NODE :", n)
+    print("-----------------------------------------------------------------------------------------")
+        
+    if status[n] >= 0:
+        minCost, childNodeList = computeMinimumCostChildNodes(n, h, graph)
+        print(minCost, childNodeList)
+        
+        h[n] = minCost
+        status[n] = len(childNodeList)
+        solved = True
+        
+        for childNode in childNodeList:
+            parent[childNode] = n
+            if status[childNode] != -1:
+                solved = solved & False
+        
+        if solved==True:
+            status[n] = -1
+            solutionGraph[n] = childNodeList
+        
+        if n != start:
+            aostar(status, h, graph, solutionGraph, parent[n], True)
+            
+        if backTracking == False:
+            for childNode in childNodeList:
+                status[childNode] = 0
+                aostar(status, h, graph, solutionGraph, childNode, False)
+h = {'A': 1, 'B': 6, 'C': 2, 'D': 12, 'E': 2, 'F': 1, 'G': 5, 'H': 7, 'I': 7, 'J': 1}
 
-h2 = {'A': 1, 'B': 6, 'C': 12, 'D': 10, 'E': 4, 'F': 4, 'G': 5, 'H': 7}  
-graph2 = {                                         
-    'A': [[('B', 1), ('C', 1)], [('D', 1)]],       
-    'B': [[('G', 1)], [('H', 1)]],                
-    'D': [[('E', 1), ('F', 1)]]                   
+graph = {
+    'A': [[('B', 1), ('C', 1)], [('D', 1)]],
+    'B': [[('G', 1)], [('H', 1)]],
+    'C': [[('J', 1)]],
+    'D': [[('E', 1), ('F', 1)]],
+    'G': [[('I', 1)]],
+    'I': [],
+    'J': []
 }
 
-G2 = Graph(graph2, h2, 'A')                       
-G2.applyAOStar()                                  
-G2.printSolution()                                
+start = 'A'
+
+parent = {}
+
+status = {
+    'A': 0,
+    'B': 0,
+    'C': 0,
+    'D': 0,
+    'E': 0,
+    'F': 0,
+    'G': 0,
+    'H': 0,
+    'I': 0,
+    'J': 0
+}
+
+solution = {}
+
+aostar(status, h, graph, solution, start, False)
+
+print("FOR GRAPH SOLUTION, TRAVERSE THE GRAPH FROM THE START NODE:", start)
+print("------------------------------------------------------------")
+print(solution)
+print("------------------------------------------------------------")
